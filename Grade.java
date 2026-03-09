85d702b0176897e5058d3f9cda5397b04cc93bc3
ritwikakundu45-hue <ritwikakundu45@gmail.com>
2026-03-09 14:30:28 +0000

    add a java program to find the grade

---
+++ Grade.java
@@ -0,0 +1,30 @@
+class Grade {
+    double percentage;
+
+    Grade(double p) {
+        percentage = p;
+    }
+
+    void calculateGrade() {
+        if (percentage >= 90)
+            System.out.println("Grade A");
+        else if (percentage >= 80)
+            System.out.println("Grade B");
+        else if (percentage >= 70)
+            System.out.println("Grade C");
+        else if (percentage >= 60)
+            System.out.println("Grade D");
+        else if (percentage >= 40)
+            System.out.println("Grade E");
+        else
+            System.out.println("Grade F");
+    }
+
+    public static void main(String[] args) {
+        Grade obj = new Grade(85);
+        obj.calculateGrade();
+    }
+}
+
+
+
