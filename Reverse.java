8334b812fb2637127dbfeeed42ef35a7a0196c1d
ritwikakundu45-hue <ritwikakundu45@gmail.com>
2026-04-02 14:25:04 +0000

    add a java program to reverse a string

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Reverse{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        String[] words = str.split(" ");
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i] + " ");
+        }
+    }
+}
