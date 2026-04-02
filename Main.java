6a3dfcc4f5ae6b631bc3773ad2097493844468ac
ritwikakundu45-hue <ritwikakundu45@gmail.com>
2026-04-02 14:12:29 +0000

    add a java progfram to define a class tender

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Tender {
+    double cost;
+    String company;
+
+    void getData(Scanner sc) {
+        System.out.print("Enter company name: ");
+        company = sc.next();
+        System.out.print("Enter cost: ");
+        cost = sc.nextDouble();
+    }
+}
+
+public class tend{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Tender[] t = new Tender[5];
+
+        for (int i = 0; i < 5; i++) {
+            t[i] = new Tender();
+            t[i].getData(sc);
+        }
+
+        double min = t[0].cost;
+        String minCompany = t[0].company;
+
+        for (int i = 1; i < 5; i++) {
+            if (t[i].cost < min) {
+                min = t[i].cost;
+                minCompany = t[i].company;
+            }
+        }
+
+        System.out.println("Company with minimum cost: " + minCompany);
+    }
+}
